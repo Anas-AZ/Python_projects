53d0a3e4f2a2e5318d13106e8dfd01b9be41b53f
Anas Ahammed <46455610+Anas-AZ@users.noreply.github.com>
2022-01-15 12:35:43 +0530

    Create paddle.py

---
+++ Intermediate/2- Ping Pong/paddle.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, pos):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)  # stretches by 5x20 and 1x20 (20x20 is initial)
+        self.color("white")
+        self.penup()
+        self.goto(pos)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
+
+
+
+
+
+
+
+
+
+
